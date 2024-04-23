096d827ba741fc0bc1c6c4d71a23e6fcc1379783
soggeebread <117228390+soggeebread@users.noreply.github.com>
2024-04-22 21:47:28 -0500

    Imports, reload, and docstring cleanup. Redo of some logic handling of Asset data and pushed to unique Class object named ValkyrieAsset and ValkyrieAssetVariant instead of nested dictionaries. Fixed UI leaving old asset details in panels when nothing is selected.

--- src/tools/Asset/AssetManager/src/gui/asset_widget_item.py
+++ src/tools/Asset/AssetManager/src/gui/asset_widget_item.py
@@ -1,44 +1,16 @@
 # Copyright (C) 2023 Robert Wiese - All Rights Reserved.
-"""Custom Asset Widget QListWidgetItem class.
-
-The asset_details dict often used in this file is of the following format:
-{
-    "asset_name": "AssetName",
-    "asset_path": "../Path/to/asset/root/folder,
-    "asset_preview": "../Path/to/asset/root/preview.jpg,
-    "asset_metadata": "../Path/to/asset/root/AssetName_metadata.json,
-    "asset_variations": {
-        "<variationName>": {
-            "published_versions": {
-                "<version>": {
-                    "maya_file": "../Path/to/published_maya_file.mb",
-                    "version_preview": "../Path/to/published_preview_image.jpg"
-                }
-            }
-            "apb_versions": [
-                "../Path/to/apb_file_v001.mb",
-                "../Path/to/apb_file_v002.mb"
-            ]
-        }
-    }
-}
-"""
+"""Custom Asset Widget QListWidgetItem class."""
 # Can't find PySide2 modules pylint: disable=I1101
 
 import logging
 import os
-from importlib import reload
 
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QMainWindow
 
 from Core import core_paths as cpath
-from Core.util import file_util_tools as fut
 
-from Asset.AssetManager.src.util import asset_manager_utils as amu
-
-reload(fut)
-reload(amu)
+from Asset.AssetManager.src.util import valkyrie_asset as val
 
 # Main paths
 MAIN_PATHS = cpath.core_paths()
@@ -58,7 +30,7 @@ class AssetWidgetItem(QtWidgets.QListWidgetItem):
     def __init__(
         self,
         parent_object: QtWidgets.QListWidget,
-        asset_details: dict,
+        asset_object: val.ValkyrieAsset,
         tool_object: QMainWindow,
     ):
         super().__init__(parent_object)
@@ -66,33 +38,22 @@ class AssetWidgetItem(QtWidgets.QListWidgetItem):
         self.tool_object = tool_object
         self.root_object = tool_object.root
 
-        self.asset_details = asset_details
+        self.asset_object = asset_object
         self.asset_name: str
         self.asset_path: str
         self.asset_preview: str
         self.asset_metadata: str
         self.asset_variations: dict
 
-        self.set_asset_details(self.asset_details)
+        self.set_asset_details()
 
-    def set_asset_details(self, asset_details: dict):
-        """Set list widget item's asset details.
-
-        Args:
-            asset_details (dict): Asset details which include the following:
-                {
-                    "asset_name": str,
-                    "asset_path": str,
-                    "asset_preview": str,
-                    "asset_metadata": str,
-                    "asset_variations" dict
-                }
-        """
-        self.asset_name = asset_details["asset_name"]
-        self.asset_path = asset_details["asset_path"]
-        self.asset_preview = asset_details["asset_preview"]
-        self.asset_metadata = asset_details["asset_metadata"]
-        self.asset_variations = asset_details["asset_variations"]
+    def set_asset_details(self):
+        """Set list widget item's asset details."""
+        self.asset_name = self.asset_object.get_asset_name()
+        self.asset_path = self.asset_object.get_asset_path()
+        self.asset_preview = self.asset_object.get_asset_preview_path()
+        self.asset_metadata = self.asset_object.get_asset_metadata_path()
+        self.asset_variations = self.asset_object.get_all_asset_variants()
 
     def get_variation_names(self):
         """Get asset variation names.
@@ -119,7 +80,7 @@ class AssetWidgetItem(QtWidgets.QListWidgetItem):
                 }
         """
         LOG.debug("Variation Dictionary: %s", self.asset_variations)
-        if "published_versions" not in self.asset_variations[variation]:
+        if len(self.asset_variations[variation].get_published_versions()) == 0:
             LOG.warning(
                 "No published maya file versions found in: %s",
                 self.asset_variations[variation],
@@ -127,9 +88,9 @@ class AssetWidgetItem(QtWidgets.QListWidgetItem):
             return {}
 
         maya_file_details = {}
-        for version, version_details in self.asset_variations[variation][
-            "published_versions"
-        ].items():
+        for version, version_details in (
+            self.asset_variations[variation].get_published_versions().items()
+        ):
             LOG.info(
                 "Getting %s variation published maya file for version: %s...",
                 variation,
--- src/tools/Asset/AssetManager/src/util/asset_manager_utils.py
+++ src/tools/Asset/AssetManager/src/util/asset_manager_utils.py
@@ -1,52 +1,21 @@
 # Copyright (C) 2023 Robert Wiese - All Rights Reserved.
-"""Asset Manager general utility functions.
-
-The asset_details dict often used in this file is of the following format:
-{
-    "asset_name": "AssetName",
-    "asset_path": "../Path/to/asset/root/folder,
-    "asset_preview": "../Path/to/asset/root/preview.jpg,
-    "asset_metadata": "../Path/to/asset/root/AssetName_metadata.json,
-    "asset_variations": {
-        "<variationName>": {
-            "published_versions": {
-                "<version>": {
-                    "maya_file": "../Path/to/published_maya_file.mb",
-                    "version_preview": "../Path/to/published_preview_image.jpg"
-                }
-            }
-            "apb_versions": [
-                "../Path/to/apb_file_v001.mb",
-                "../Path/to/apb_file_v002.mb"
-            ]
-        }
-    }
-}
-"""
+"""Asset Manager general utility functions."""
 # Can't find PySide2 modules pylint: disable=I1101
 
 import logging
 import os
-import re
 
 from PySide6 import QtGui
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QMainWindow
 
-# from Core import core_paths as cpath
 from Core.util import file_util_tools as fut
-from Core.util import string_util_tools as sut
 from Core.util import project_util_tools as prj
 
 from Asset.AssetManager.src.gui import asset_list_utils as alu
 from Asset.AssetManager.src.gui import asset_widget_item as awi
 from Asset.AssetManager.src.gui import file_widget_item as fwi
-
-from importlib import reload
-
-reload(fut)
-reload(alu)
-reload(sut)
+from Asset.AssetManager.src.util import valkyrie_asset as val
 
 LOADER = QUiLoader()
 
@@ -74,15 +43,15 @@ def get_asset_preview(asset_path: str, asset_name: str):
     return asset_preview_path
 
 
-def get_asset_variations(asset_details: dict):
+def get_asset_variations(asset_object: val.ValkyrieAsset):
     """Get all variations of a specific Asset.
 
     Args:
-        asset_details (dict): Asset dictionary to be modified with gathered data.
+        asset_object (dict): Asset dictionary to be modified with gathered data.
     """
-    LOG.debug("ASSET PATH TO GET VARIATIONS: %s", asset_details["asset_path"])
+    LOG.debug("ASSET PATH TO GET VARIATIONS: %s", asset_object.get_asset_path())
     variations = fut.get_files_or_folders(
-        asset_details["asset_path"], False, True, "[a-zA-Z]+"
+        asset_object.get_asset_path(), False, True, "[a-zA-Z]+"
     )
     if variations is None:
         return
@@ -90,106 +59,8 @@ def get_asset_variations(asset_details: dict):
     for variation_path in variations:
         LOG.debug("VARIATION PATH: %s", variation_path)
         variation_name = variation_path.split("/")[-1]
-        asset_details["asset_variations"][variation_name] = get_variation_details(
-            variation_path
-        )
 
-
-def get_variation_details(variation_path: str):
-    """Get variation asset details.
-
-    Details include all published and APB versions of the variant.
-
-    Args:
-        variation_path (str): Path to variation root folder.
-
-    Returns:
-        dict: Details about the variation's published and APB versions in the following
-            format (example):
-            {
-                "published_versions":{
-                    "v001": {
-                    "maya_file": "/path/to/published_maya_file.mb",
-                    "version_preview": "path/to/published_preview_image.jpg"
-                },
-                "apb_versions":[
-                    "path/to/apb_file_v001.mb",
-                    "path/to/apb_file_v002.mb",
-                ]
-            }
-    """
-    variation_details = {
-        "published_versions": get_published_versions(variation_path),
-        "apb_versions": get_abp_versions(variation_path),
-    }
-    LOG.debug("VARIATION DETAILS: %s", variation_details)
-    return variation_details
-
-
-def get_published_versions(variation_path: str):
-    """Get published maya file and preview paths.
-
-    Args:
-        variation_path (str): Root asset directory path.
-
-
-    Returns:
-        dict: Dictionary of published version details (example):
-            {
-                "v001": {
-                    "maya_file": "/path/to/published_maya_file.mb",
-                    "version_preview": "path/to/published_preview_image.jpg"
-                }
-            }
-    """
-    LOG.info("Retrieving published versions...")
-    # Get published version folders for variation
-    asset_versions = fut.get_files_or_folders(
-        f"{variation_path}/Publish", False, True, "v[0-9]{3,4}"
-    )
-
-    if asset_versions is None:
-        return {}
-
-    published_versions = {}
-    for version_path in asset_versions:
-        version = version_path.split("/")[-1]
-        published_versions[version] = {"maya_file": str, "version_preview": str}
-        for item in os.listdir(version_path):
-            item_path = f"{version_path}/{item}"
-            LOG.debug("ITEM NAME: %s", item)
-            if re.match(PROJECT_CONFIGS["asset_publish_preview_regex"], item):
-                LOG.debug("FOUND VERSION PREVIEW")
-                published_versions[version]["version_preview"] = item_path
-
-            if re.match(PROJECT_CONFIGS["asset_published_regex"], item):
-                published_versions[version]["maya_file"] = item_path
-
-    return published_versions
-
-
-def get_abp_versions(variation_path: str):
-    """Get APB/wip maya file paths.
-
-    Args:
-        variation_path (str): Path to variation root folder.
-
-    Returns:
-        list(str): List of paths to apb/wip maya files for variation.
-    """
-    LOG.info("Retrieving apb versions...")
-    # Get published version folders for variation
-    asset_versions = fut.get_files_or_folders(
-        f"{variation_path}/APB/Maya",
-        True,
-        True,
-        PROJECT_CONFIGS["asset_pre_build_maya_file_regex"],
-    )
-
-    if asset_versions is None:
-        return []
-
-    return asset_versions
+        asset_object.add_asset_variant(variation_name, variation_path)
 
 
 def update_variation_options(item_selected: awi.AssetWidgetItem):
@@ -240,7 +111,7 @@ def update_apb_versions(item_selected: awi.AssetWidgetItem):
     item_selected.root_object.list_apb_files.clear()
 
     variation = item_selected.root_object.cbo_variations.currentText()
-    for maya_file_path in item_selected.asset_variations[variation]["apb_versions"]:
+    for maya_file_path in item_selected.asset_variations[variation].get_apb_versions():
         LOG.debug("APB Maya file: %s", maya_file_path)
         version_details = {"maya_file": maya_file_path, "version_preview": ""}
         alu.add_file_widget(item_selected, version_details, False)
@@ -343,7 +214,3 @@ def shorten_category_name(long_name: str):
         short_name = "veh"
 
     return short_name
-
-
-def create_asset_file_name():
-    pass
